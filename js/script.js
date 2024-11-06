1aedd5d0aeb8d1809a66afe4e6cf5d9a06c7ebe8
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-11-06 15:36:54 +0100

    add api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("JS OK");
+// # elements
+
+const mailContainer = document.getElementById("email-list-container");
+let generatedMail;
+
+// # API
+
+fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
